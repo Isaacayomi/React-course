da84f964ed532ef93d9e5f06d89ce895a0fa3fff
Isaac Ayomide <okunlolaa3@outlook.com>
2025-01-22 20:39:21 +0100

    more components created for the pizza app

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -50,10 +50,9 @@ const pizzaData = [
 function App() {
   return (
     <div>
-      <h1> Hello world </h1>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Header />
+      <Menu />
+      <Footer />
     </div>
   );
 }
@@ -65,6 +64,28 @@ function Pizza() {
       <h2>Pizza Spinaci</h2>
       <p>Tomato, mozarella, spinach, and ricotta cheese</p>
     </div>
+  );
+}
+
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our menu</h2>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>{new Date().toLocaleTimeString()} We are currently open!</footer>
   );
 }
 
