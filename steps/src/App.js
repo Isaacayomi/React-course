cf88e096dd1461989560887e278578309a9a69bc
Isaac Ayomide <okunlolaa3@outlook.com>
2025-01-28 22:20:56 +0100

    created a state variable with useState

--- steps/src/App.js
+++ steps/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
@@ -5,7 +7,58 @@ const messages = [
 ];
 
 export default function App() {
-  const step = 1;
+  const [step, setStep] = useState(1);
+
+  function handlePrevious() {
+    if (step > 1) setStep(step - 1);
+  }
+
+  function handleNext() {
+    if (step < 3) setStep(step + 1);
+  }
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+
+      <p className="message">
+        Step: {step}: {messages[step - 1]}
+      </p>
+
+      <div className="buttons">
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePrevious}
+        >
+          Previous
+        </button>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNext}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
+{
+  /* 
+
+export default function App() {
+  const [step, setStep] = useState(1);
+
+  function handlePrevious() {
+    alert("Previous");
+  }
+
+  function handleNext() {
+    alert("Next");
+    setStep(step + 1);
+  }
   return (
     <div className="steps">
       <div className="numbers">
@@ -19,8 +72,8 @@ export default function App() {
       </p>
 
       <div className="buttons">
-        <Buttons name="Next" />
-        <Buttons name="Previous" />
+        <Buttons name="Next" onClick={handleNext} />
+        <Buttons name="Previous" onClick={handlePrevious} />
       </div>
     </div>
   );
@@ -35,18 +88,13 @@ function NumberButtons(props) {
 }
 
 function Buttons(props) {
-  function handleButtons() {
-    alert(props.name);
-  }
-
   return (
     <div className="buttons">
-      <button
-        style={{ backgroundColor: "#7950f2", color: "#fff" }}
-        onClick={handleButtons}
-      >
+      <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
         {props.name}
       </button>
     </div>
   );
+}
+*/
 }
