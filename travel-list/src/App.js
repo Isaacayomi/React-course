7e3d42d35e38f8a97b02cff9b8fcc750e5527341
Isaac Ayomide <okunlolaa3@outlook.com>
2025-02-06 21:15:23 +0100

    update

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -2,6 +2,7 @@ const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
   { id: 3, description: "shoes", quantity: 12, packed: true },
+  { id: 4, description: "laptop", quantity: 1, packed: true },
 ];
 
 export default function App() {
@@ -16,14 +17,24 @@ export default function App() {
 }
 
 function Logo() {
-  return <h1>Far Away</h1>;
+  return <h1>FAR AWAY</h1>;
 }
 
 function Form() {
   return (
-    <div className="add-form">
+    <form className="add-form">
       <h3>What do you need for your trip?</h3>
-    </div>
+      <select>
+        {/* Creating an option of 1-20 using the Array.from method */}
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form>
   );
 }
 
@@ -32,7 +43,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} key={item.description} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
@@ -42,9 +53,11 @@ function PackingList() {
 function Item({ item }) {
   return (
     <li>
-      {" "}
-      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
-        {item.quanity} {item.description}
+      <span
+        style={item.packed === true ? { textDecoration: "line-through" } : {}}
+      >
+        {" "}
+        {item.quantity} {item.description}
       </span>
       <button style={{ color: "red" }}>&times;</button>
     </li>
@@ -53,8 +66,8 @@ function Item({ item }) {
 
 function Stats() {
   return (
-    <footer className="stats">
-      <em> You have X items on your list, and you already packed X (X%)</em>
+    <footer>
+      <em>You have X items on your list, and you already packed X (X%)</em>
     </footer>
   );
 }
